92658b7fc7157a955f3b50c97f2758056f8a363a
Ali Prasla <ali@wellsuited.com>
2019-05-16 10:06:16 -0500

    Cleaned up basic code by running through a linter

--- src/app/database/__init__.py
+++ src/app/database/__init__.py
@@ -8,42 +8,48 @@ from pathlib import Path
 
 
 def load_training_data(data_name):
-	"""
-	Loads training data from directory
-	"""
-	f_path = _get_training_data_directory()/data_name
+    """
+    Loads training data from directory
+    """
+    f_path = _get_training_data_directory()/data_name
 
-	return _load_pickle_file(f_path)
-
-def save_model(model_object,model_name:str):
-	"""
-	Save model file to local "data" directory
-	"""
-	f_path = _get_model_directory() / model_name
-	_save_pickle_file(model_object,f_path)
-
-def load_model(model_name:str):
-	f_path = _get_model_directory() / model_name
-	return _load_pickle_file(f_path)
+    return _load_pickle_file(f_path)
 
 
+def save_model(model_object, model_name: str):
+    """
+    Save model file to local "data" directory
+    """
+    f_path = _get_model_directory() / model_name
+    _save_pickle_file(model_object, f_path)
 
+
+def load_model(model_name: str):
+    """
+    Loads a pickled model file from a directory. NOTE: Change to handle multiple data sources
+    """
+    f_path = _get_model_directory() / model_name
+    return _load_pickle_file(f_path)
 
 
 ############################
 # PRIVATE METHODS
 ############################
 def _get_training_data_directory():
-	return _get_data_directory() / "training_data"
+    return _get_data_directory() / "training_data"
+
 
 def _get_model_directory():
-	return _get_data_directory() / "models"
+    return _get_data_directory() / "models"
+
 
 def _get_data_directory():
-	return Path("data")
+    return Path("data")
+
 
 def _load_pickle_file(f_path):
-	return pickle.load(open(f_path,'rb'))
+    return pickle.load(open(f_path, 'rb'))
 
-def _save_pickle_file(model_object,f_path):
-	pickle.dump(model_object,open(f_path,'wb'))
+
+def _save_pickle_file(model_object, f_path):
+    pickle.dump(model_object, open(f_path, 'wb'))
--- src/app/models/cnn.py
+++ src/app/models/cnn.py
@@ -1,143 +1,137 @@
-from keras.layers import Input, Dense, Conv2D, MaxPooling2D, BatchNormalization, Dropout,Reshape, Flatten
+"""
+This file contains Convolutional Neural Network based classes for MNIST
+"""
+from keras.layers import Input, Dense, Conv2D, MaxPooling2D, Reshape, Flatten
 from keras.models import Model
 from keras.utils import to_categorical
-from sklearn.model_selection import train_test_split
 import numpy as np
 
 
-class MNIST_Classifer:
-	"""
-	Author: Ali Prasla
+class MNISTClassifer:
+    """
+    Author: Ali Prasla
 
-	This class presents a wrapper for a Keras MNIST classifer. 
+    This class presents a wrapper for a Keras MNIST classifer.
 
-	It contains the CNN architecture necessary to accurately make predictions. This is an example and is a relatively rigid architecture
+    It contains the CNN architecture necessary to accurately make predictions.
+        This is an example and is a relatively rigid architecture
 
-	"""
+    """
 
-	# in this example we are using the basic MNIST Dimensions
-	feature_length = 784
-	num_classes = 10
+    # in this example we are using the basic MNIST Dimensions
+    feature_length = 784
+    num_classes = 10
 
-	def __init__(self,
-					conv_layer_one_filters = 20,
-					conv_layer_one_kernel_size = (2,2),
-					conv_layer_one_activation = 'relu',
-					max_pooling_layer_one_pool_size = (2,2),
-					dense_layer_one_num_units = 20,
-					dense_layer_one_activation = 'relu',
-					dense_layer_two_num_units = 20,
-					dense_layer_two_activation = 'relu',
-					optimizer = 'adam',
-					loss = 'categorical_crossentropy',
-					metrics = ['accuracy']):
+    def __init__(self,
+                 conv_layer_one_filters=20,
+                 conv_layer_one_kernel_size=(2, 2),
+                 conv_layer_one_activation='relu',
+                 max_pooling_layer_one_pool_size=(2, 2),
+                 dense_layer_one_num_units=20,
+                 dense_layer_one_activation='relu',
+                 dense_layer_two_num_units=20,
+                 dense_layer_two_activation='relu',
+                 optimizer='adam',
+                 loss='categorical_crossentropy',
+                 metrics=['accuracy']):
 
-		# define hyper parameter
-					
-		self.conv_layer_one_filters = conv_layer_one_filters
-		self.conv_layer_one_kernel_size = conv_layer_one_kernel_size
-		self.conv_layer_one_activation = 'relu'
+        # define hyper parameter
 
-		self.max_pooling_layer_one_pool_size = (2,2)
+        self.conv_layer_one_filters = conv_layer_one_filters
+        self.conv_layer_one_kernel_size = conv_layer_one_kernel_size
+        self.conv_layer_one_activation = conv_layer_one_activation
 
+        self.max_pooling_layer_one_pool_size = max_pooling_layer_one_pool_size
 
-		self.dense_layer_one_num_units = 20
-		self.dense_layer_one_activation = 'relu'
+        self.dense_layer_one_num_units = dense_layer_one_num_units
+        self.dense_layer_one_activation = dense_layer_one_activation
 
+        self.dense_layer_two_num_units = dense_layer_two_num_units
+        self.dense_layer_two_activation = dense_layer_two_activation
 
-		self.dense_layer_two_num_units = 20
-		self.dense_layer_two_activation = 'relu'
+        self.optimizer = optimizer
+        self.loss = loss
+        self.metrics = metrics
 
-		self.optimizer = 'adam'
-		self.loss = 'categorical_crossentropy'
-		self.metrics = ['accuracy']
+        self.model = None
 
-		self.model = None
+    def fit(self, x_train, y_train, batch_size: int, **kwargs):
+        """
+        Fits the underlying keras model
 
-	def fit(self,X,y,batch_size:int,**kwargs):
-		"""
-		Fits the underlying keras model
+        Args:
+                x_train (Array like) : X Training Data
+                y_train (Array like) : y target values
+                batch_size (int) :  number of sample per gradient update
 
-		Args:
-			X (Array like) : X Training Data
-			y (Array like) : y target values
-			batch_size (int) :  number of sample per gradient update
+                **kwargs : additional parameters passed to the keras .fit method
 
-			**kwargs : additional parameters passed to the keras .fit method
+        """
 
-		"""
+        if y_train.ndim == 1:
+            y_one_hot = to_categorical(y_train, num_classes=self.num_classes)
+        else:
+            assert y_one_hot.shape[1] == self.num_classes
+            y_one_hot = y_train
 
-		if y.ndim == 1:
-			y_one_hot = to_categorical(y,num_classes=self.num_classes)
-		else:
-			assert y_one_hot.shape[1] == self.num_classes
-			y_one_hot = y
-			
-		
-		self.model = self._get_untrained_model()
+        self.model = self._get_untrained_model()
 
-		self.model.compile(optimizer=self.optimizer,loss= self.loss,metrics = self.metrics)
-		
-		self.model.fit(x=X,y=y_one_hot,
-						batch_size=batch_size,
-						**kwargs)
+        self.model.compile(optimizer=self.optimizer,
+                           loss=self.loss, metrics=self.metrics)
 
-	def predict(self,X):
-		"""
-		Runs model prediction
+        self.model.fit(x=x_train, y=y_one_hot,
+                       batch_size=batch_size,
+                       **kwargs)
 
-		Args:
-			X (Array like) : Data upon which you want to make predictions
-		"""
+    def predict(self, x_predict):
+        """
+        Runs model prediction
 
-		# run prediction
-		y_pred = self.model.predict(X)
+        Args:
+                x_predict (Array like) : Data upon which you want to make predictions
+        """
 
-		# convert softmax floats into 
-		construct = np.argmax(y_pred,axis = 1)
+        # run prediction
+        y_pred = self.model.predict(x_predict)
 
-		return construct.astype(str)
+        # convert softmax floats into
+        construct = np.argmax(y_pred, axis=1)
 
+        return construct.astype(str)
 
-	def _get_untrained_model(self):
+    def _get_untrained_model(self):
+        """
+        Defines Model Architecture using Keras' Functional API
 
-		"""
-		Defines Model Architecture using Keras' Functional API
+        """
 
-		"""
+        # input layer with dimensions
 
-		# input layer with dimensions
+        # assuming square images for MNIST
+        num_image_rows = int(np.sqrt(self.feature_length))
 
-		# assuming square images for MNIST 
-		num_image_rows = int(np.sqrt(self.feature_length))
+        input_layer = Input(shape=(self.feature_length,))
 
-		input_layer = Input(shape=(self.feature_length,))
+        model = Reshape((num_image_rows, num_image_rows, 1))(input_layer)
 
+        # define convolutional layers
 
-		model = Reshape((num_image_rows,num_image_rows,1))(input_layer)
+        model = Conv2D(filters=self.conv_layer_one_filters,
+                       kernel_size=self.conv_layer_one_kernel_size,
+                       activation=self.conv_layer_one_activation)(model)
 
-		
-		# define convolutional layers
+        model = MaxPooling2D(
+            pool_size=self.max_pooling_layer_one_pool_size)(model)
 
-		model = Conv2D(filters = self.conv_layer_one_filters,
-							kernel_size = self.conv_layer_one_kernel_size,
-							activation=self.conv_layer_one_activation)(model)
-		
-		model = MaxPooling2D(pool_size = self.max_pooling_layer_one_pool_size)(model)
+        model = Flatten()(model)
 
-		model = Flatten()(model)
+        model = Dense(units=self.dense_layer_one_num_units,
+                      activation=self.dense_layer_one_activation)(model)
 
-		model = Dense(units = self.dense_layer_one_num_units,
-						activation=self.dense_layer_one_activation)(model)
+        model = Dense(units=self.dense_layer_two_num_units,
+                      activation=self.dense_layer_two_activation)(model)
 
+        prediction_layer = Dense(
+            units=self.num_classes, activation="softmax", name="output_layer")(model)
 
-		model = Dense(units = self.dense_layer_two_num_units,
-						activation=self.dense_layer_two_activation)(model)
-
-
-		prediction_layer = Dense(units=self.num_classes,activation="softmax",name="output_layer")(model)
-
-		return Model(inputs=input_layer,outputs=prediction_layer)
-
-
-			
+        return Model(inputs=input_layer, outputs=prediction_layer)
